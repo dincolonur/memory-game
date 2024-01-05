57a8a032c3df0e81dcc15d1e5e31816bf6111199
Onur Dincol <onurdincol@Onurs-MacBook-Pro.local>
2024-01-04 22:22:41 -0800

    First Commit

---
+++ my-app/src/Components/Card.js
@@ -0,0 +1,25 @@
+import Educative from '../Images/smiley-icon.png';
+
+const Card = ({ card, flipped, disabled, handleChoice }) => {
+  const handleClick = () => {
+    if (!disabled) {
+      handleChoice(card);
+    }
+  };
+
+  return (
+    <div className="card" key={card.id}>
+      <div className={flipped ? "flipped" : ""}>
+        <img src={card.src} alt="card front" className="front" />
+        <img
+          onClick={handleClick}
+          src={Educative}
+          alt="card back"
+          className="back"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Card;
